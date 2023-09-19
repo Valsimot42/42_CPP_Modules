ed5f60c38805ff62c64eee37f097f0a7ebe1f936
tbolkova <tbolkova@student.42wolfsburg.de>
2023-09-19 11:22:41 +0200

    added ex03 groundwork

---
+++ Module_01/ex03/inc/Weapon.hpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Weapon.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tbolkova <tbolkova@student.42wolfsburg.d>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/19 11:16:48 by tbolkova          #+#    #+#             */
+/*   Updated: 2023/09/19 11:16:48 by tbolkova         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef WEAPON_H
+# define WEAPON_H
+
+#include <iostream>
+
+class Weapon
+{
+public:
+	Weapon();
+	~Weapon();
+
+private:
+	std::string _type;
+};
+
+
+#endif
