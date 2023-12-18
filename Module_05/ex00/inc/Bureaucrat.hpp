a5de35cdbb53081cffc7c27de4ace0daf4d0078f
tbolkova <tbolkova@student.42wolfsburg.de>
2023-12-18 14:56:51 +0100

    finished

--- Module_05/ex00/inc/Bureaucrat.hpp
+++ Module_05/ex00/inc/Bureaucrat.hpp
@@ -10,3 +10,48 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
+
+#include <iostream>
+
+#define red "\033[1;31m"
+#define green "\033[1;32m"
+#define yellow "\033[1;33m"
+#define blue "\033[1;34m"
+#define reset "\033[0m"
+
+
+class Bureaucrat {
+public:
+	Bureaucrat(std::string name, int grade);
+	Bureaucrat(const Bureaucrat &copy);
+	Bureaucrat &operator=(const Bureaucrat &copy);
+	~Bureaucrat();
+
+	std::string getName() const;
+	int getGrade() const;
+
+	void incrementGrade();
+	void decrementGrade();
+
+
+private:
+	const std::string _name;
+	int _grade;
+
+
+class GradeTooHighException : public std::exception {
+	public:
+		virtual const char *what() const throw();
+	};
+
+class GradeTooLowException : public std::exception {
+	public:
+		virtual const char *what() const throw();
+	};
+};
+
+std::ostream &operator<<(std::ostream &output, const Bureaucrat &input);
+
+#endif
--- Module_05/ex00/src/main.cpp
+++ Module_05/ex00/src/main.cpp
@@ -10,3 +10,72 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../inc/Bureaucrat.hpp"
+
+int main() {
+	std::cout << blue << "---INCREMENT/DECREMENT---" << reset << std::endl;
+
+	try {
+		Bureaucrat Alpha("Freya", 150);
+
+		std::cout << green << Alpha << reset;
+		for (int i = 0; i < 5; i++){
+			Alpha.incrementGrade();
+			std::cout << yellow << Alpha << reset;
+		}
+		for (int i = 0; i < 6; i++){
+			Alpha.decrementGrade();
+			std::cout << yellow << Alpha << reset;
+		}
+		for (int i = 0; i < 10; i++){
+			Alpha.incrementGrade();
+			std::cout << Alpha;
+		}
+	}
+	catch (std::exception &e) {
+		std::cerr << red << e.what() << reset << std::endl;
+	}
+
+	std::cout << blue << "---GRADE TOO HIGH---" << reset << std::endl;
+
+	try {
+		Bureaucrat Beta("Fratley", 1);
+
+		std::cout << green << Beta << reset;
+		Beta.incrementGrade();
+		std::cout << yellow << Beta << reset;
+	}
+	catch (std::exception &e) {
+		std::cerr << red << e.what() << reset << std::endl;
+	}
+
+	std::cout << blue << "---GRADE TOO LOW---" << reset << std::endl;
+
+	try {
+		Bureaucrat Gamma("Puck", 150);
+
+		std::cout << green << Gamma << reset;
+		Gamma.decrementGrade();
+		std::cout << yellow << Gamma << reset;
+	}
+	catch (std::exception &e) {
+		std::cerr << red << e.what() << reset << std::endl;
+	}
+
+	std::cout << blue << "---COPY CONSTRUCTOR---" << reset << std::endl;
+
+		Bureaucrat Delta("Kuja", 69);
+		Bureaucrat Epsilon(Delta);
+		Bureaucrat Zeta = Delta;
+
+		std::cout << green << Delta << reset;
+
+		Epsilon.incrementGrade();
+		std::cout << yellow << Epsilon << reset;
+
+		Zeta.decrementGrade();
+		std::cout << yellow << Zeta << reset;
+
+	std::cout << blue << "---END---" << reset << std::endl;
+}
+
