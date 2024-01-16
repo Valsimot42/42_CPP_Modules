f508778cb9374c5a46f5f5c7ea7783717e7f4301
Tomislav Bolkovac <tbolkova@c2r14s1.42wolfsburg.de>
2024-01-16 17:39:29 +0100

    form.hpp set

--- Module_05/ex01/inc/Form.hpp
+++ Module_05/ex01/inc/Form.hpp
@@ -6,7 +6,7 @@
 /*   By: tbolkova <tbolkova@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/16 13:18:24 by tbolkova          #+#    #+#             */
-/*   Updated: 2024/01/16 15:57:45 by tbolkova         ###   ########.fr       */
+/*   Updated: 2024/01/16 17:38:01 by tbolkova         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,37 @@
 
 #include <iostream>
 #include "Bureaucrat.hpp"
+
+class Form {
+public:
+    Form(std::string name, int gradeToSign, int gradeToExecute);
+    Form(const Form &copy);
+    Form &operator=(const Form &copy);
+    ~Form();
+    
+    std::string getName() const;
+    int getSignGrade() const;
+    int getExecuteGrade() const;
+
+    bool beSigned(Bureaucrat *bureaucrat) const;
+
+private:
+    const std::string _name;
+    bool _isSigned;
+    int _gradeToSign;
+    int _gradeToExecute;
+
+class GradeTooHighException : public std::exception {
+    public:
+        virtual const char *what() const throw();
+    };
+
+class GradeTooLowException : public std::exception {
+    public:
+        virtual const char *what() const throw();
+    };
+};
+
+std::ostream &operator<<(std::ostream &output, const Form &input);
 
 #endif
